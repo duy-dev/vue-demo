0817978d0887c64d25f660cf7c81d8256646fd03
Duc Duy <ducduy.developer@gmail.com>
2022-05-05 08:38:10 +0700

    refactor: remove store language

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,6 @@
 import { createStore } from "vuex";
-import language from "./modules/language";
 import sidebar from "./modules/sidebar";
 
 export default createStore({
-  modules: { language, sidebar },
+  modules: { sidebar },
 });
